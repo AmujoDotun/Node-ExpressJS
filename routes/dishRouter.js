31300f0f7a0202f6b3207bad915c9a358312a630
AmujoDotun <samuelamujo@gmail.com>
2020-09-15 19:50:35 +0100

    you can perform all the verb on each resources id

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -31,6 +31,28 @@ dishRouter.route('/')
 
 .delete((req, res, next)=>{
     res.end('Delete all dishes!');
+})
+
+
+
+dishRouter.route('/:dishId')
+.get((req,res, next)=>{
+    res.end('Will send all the dishes to you for! ' + req.params.dishId);
+})
+
+.post((req, res, next)=>{
+    res.end('Post update is not in operation for /dishes '+ req.params.dishId)
+})
+
+.put((req,res, next)=>{
+    res.write('Updating dish '+ req.params.dishId)
+    res.end('Will Update the dish ' +req.body.name + 
+        ' with detials ' + req.body.description)
+})
+
+
+.delete((req, res, next)=>{
+    res.end('Delete dishes for ' + req.params.dishId);
 });
 
 
